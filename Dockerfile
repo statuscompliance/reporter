d2ccb10d6ba36333c576cc9fc6ad6ceac4109ac7
Alejandro Santisteban <72552837+alesancor1@users.noreply.github.com>
2023-02-12 23:43:18 +0100

    feat: Installed newrelic

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,12 @@ RUN npm install --only=prod
 ARG NODE_ENV=production
 ENV NODE_ENV $NODE_ENV
 
+# New Relic config
+RUN apk add python
+ENV NEW_RELIC_NO_CONFIG_FILE=true
+ENV NEW_RELIC_DISTRIBUTED_TRACING_ENABLED=true
+ENV NEW_RELIC_LOG=stdout
+
 ARG PORT=80
 ENV PORT $PORT
 EXPOSE $PORT
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+if (process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) require('newrelic');
+
 const governify = require('governify-commons');
 const logger = governify.getLogger().tag('index');
 
@@ -20,20 +22,20 @@ governify.init({
 });
 
 // quit on ctrl-c when running docker in terminal
-process.on('SIGINT', function onSigint () {
+process.on('SIGINT', function onSigint() {
   logger.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
   shutdown();
 });
 
 // quit properly on docker stop
-process.on('SIGTERM', function onSigterm () {
+process.on('SIGTERM', function onSigterm() {
   logger.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
   shutdown();
 });
 
 // shut down server
-function shutdown () {
-  server.undeploy(function onServerClosed (err) {
+function shutdown() {
+  server.undeploy(function onServerClosed(err) {
     if (err) {
       logger.error(err);
       process.exitCode = 1;
